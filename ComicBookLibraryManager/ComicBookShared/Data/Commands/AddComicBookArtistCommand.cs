27df3e056158addb6f35c73aa7e7a806fe07d318
Stephanie Spears <stephanie.spears1@gmail.com>
2018-08-13 00:39:55 -0700

    alt to using repos

---
+++ ComicBookLibraryManager/ComicBookShared/Data/Commands/AddComicBookArtistCommand.cs
@@ -0,0 +1,26 @@
+﻿using ComicBookShared.Models;
+
+namespace ComicBookShared.Data.Commands
+{
+    internal class AddComicBookArtistCommand
+    {
+        private readonly Context _context;
+
+        public AddComicBookArtistCommand(Context context)
+        {
+            _context = context;
+        }
+
+        public void Execute(int comicBookId, int artistId, int roleId)
+        {
+            var comicBookArtist = new ComicBookArtist()
+            {
+                ComicBookId = comicBookId,
+                ArtistId = artistId,
+                RoleId = roleId
+            };
+            _context.ComicBookArtists.Add(comicBookArtist);
+            _context.SaveChanges();
+        }
+    }
+}
